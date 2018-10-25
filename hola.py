84b41315b10d9ea1cabd3ff0a943065cfb5c67fa
jugondavidlisto <dfusteg@gmail.com>
2018-10-25 13:13:46 +0200

    Add files via upload

---
+++ hola.py
@@ -0,0 +1,12 @@
+#import sys
+#import msvcrt #CMD
+#num = sys.stdin.readline() #Numero de entrada
+#maxc = 1000 #Caramelos Max
+#maxp = 30 #Participantes Max
+#if (num > maxc):
+ #print ("Has superat els caramels")
+#elif (num > maxp):
+ #print (“Has superat els participants")
+#else:
+ #print ("hola")
+#msvcrt.getch() #Siempre en la ultima linia
---
+++ primetro.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python 
+from math import pi
+radi = 1
+perimetre = 2 * pi * radi
+print(perimetre)
